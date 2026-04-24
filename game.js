3f488206b752ecfeab64d9895ce4253acb0c0cd5
Simran Bilku <sbilku@hotmail.co.uk>
2026-04-24 17:45:54 +0100

    set up canvas with responsive sizing

--- game.js
+++ game.js
@@ -1,2 +1,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// canvas fills entire screen on device
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+// adjusts canvas size in case of device rotation or windows resize
